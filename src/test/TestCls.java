63a9b4b302ea1c64eb8c4f4c590e880c31bc5e92
Mohammed Hossain <jahangir7373hossain@gmail.com>
2021-08-22 16:41:49 -0400

    new add2

--- src/test/TestCls.java
+++ src/test/TestCls.java
@@ -5,6 +5,8 @@ public class TestCls {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("Hello Bangladesh");
+		System.out.println("New york");
+		System.out.println("New york");
+		System.out.println("New york");
 	}
-
 }
